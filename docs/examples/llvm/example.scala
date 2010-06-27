3834b3342adcbdd0d539421083b6729f3afde0a9
Geoff Reedy <geoff@programmer-monk.net>
2010-06-26 20:04:37 -0600

    Add demonstration of working variables and loops to example

--- docs/examples/llvm/example.scala
+++ docs/examples/llvm/example.scala
@@ -1,11 +1,11 @@
-class example {
+class examplec {
   val s: Short = 1
   val i: Int = 2
   val l: Long = 3
   def x: Double = s+i+l
   def z = x*2
 }
-object example extends example {
+object example extends examplec {
   val f: Float = 4
   val d: Double = 5
   val b: Byte = 6
@@ -15,7 +15,14 @@ object example extends example {
     printdouble(s+i+l)
     printdouble(x)
     printdouble(z)
+    var q = 0
+    while (q < 1000) {
+      printdouble(q)
+      q = q + 1
+    }
   }
+  def main(args: Array[String]) { main() }
 
   @native def printdouble(d: Double): Unit = ()
+  //def printdouble(d: Double): Unit = { Console.printf("%g\n", d) }
 }
