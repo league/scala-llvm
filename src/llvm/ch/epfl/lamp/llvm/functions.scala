c5cc2dfb0c9e58773b1093cc80aeadbf8287a894
Geoff Reedy <geoff@programmer-monk.net>
2010-06-26 22:12:31 -0600

    Add ability in LLVM source model to define a function with arbitrary body

--- src/llvm/ch/epfl/lamp/llvm/functions.scala
+++ src/llvm/ch/epfl/lamp/llvm/functions.scala
@@ -16,6 +16,7 @@ class LMFunction(val resultType: ConcreteType, val name: String, val args: Seq[A
   def tpe = new LMFunctionType(resultType, args.map(_.lmvar.tpe), varargs)
   def declare = new LMFunctionDecl(this)
   def define(blocks: Seq[Block]) = new LMFunctionDef(this, blocks)
+  def define(body: String) = new LMFunctionSDef(this, body)
 }
 
 class LMFunctionDecl(val fun: LMFunction) extends ModuleComp {
@@ -36,5 +37,15 @@ class LMFunctionDef(val fun: LMFunction, val blocks: Seq[Block]) extends ModuleC
     val gcSyn = fun.section.map("gc \""+_+"\"")
     val nameAndArgs = "@\""+fun.name+"\""+theargs.mkString("(",",",")")
     (Seq("define",fun.linkage.syntax,fun.visibility.syntax,fun.cconv.syntax)++fun.ret_attrs.map(_.syntax)++Seq(fun.resultType.rep,nameAndArgs)++fun.fn_attrs.map(_.syntax)++sectionSyn++alignSyn++gcSyn).mkString(" ") + blocks.map(_.syntax).mkString("{\n","\n  ","\n}")
+  }
+}
+class LMFunctionSDef(val fun: LMFunction, val body: String) extends ModuleComp {
+  def syntax = {
+    val theargs = if (fun.varargs) fun.args.map(_.defnSyn):+"..." else fun.args.map(_.defnSyn)
+    val sectionSyn = fun.section.map("section \""+_+"\"")
+    val alignSyn = fun.align.map("align "+_.toString)
+    val gcSyn = fun.section.map("gc \""+_+"\"")
+    val nameAndArgs = "@\""+fun.name+"\""+theargs.mkString("(",",",")")
+    (Seq("define",fun.linkage.syntax,fun.visibility.syntax,fun.cconv.syntax)++fun.ret_attrs.map(_.syntax)++Seq(fun.resultType.rep,nameAndArgs)++fun.fn_attrs.map(_.syntax)++sectionSyn++alignSyn++gcSyn).mkString(" ") +" {\n"+body+"\n}"
   }
 }
