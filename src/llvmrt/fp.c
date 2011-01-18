aa2522a1545d7f8b49467074924ab3156b90807a
Geoff Reedy <geoff@programmer-monk.net>
2011-01-18 01:49:23 -0700

    Add some native floating point methods

---
+++ src/llvmrt/fp.c
@@ -0,0 +1,47 @@
+#include "object.h"
+
+#include <math.h>
+
+float method__Ojava_Dlang_DFloat_MintBitsToFloat_Ascala_DInt_Rscala_DFloat(
+    struct java_lang_Object *self,
+    int32_t v)
+{
+  /* XXX is this really valid; how to do it right? */
+  return *(float*)&v;
+}
+
+double method__Ojava_Dlang_DDouble_MlongBitsToDouble_Ascala_DLong_Rscala_DDouble(
+    struct java_lang_Object *self,
+    int64_t v)
+{
+  /* XXX is this really valid; how to do it right? */
+  return *(double*)&v;
+}
+
+bool method__Ojava_Dlang_DFloat_MisNaN_Ascala_DFloat_Rscala_DBoolean(
+    struct java_lang_Object *self,
+    float v)
+{
+  return isnan(v);
+}
+
+bool method__Ojava_Dlang_DDouble_MisNaN_Ascala_DDouble_Rscala_DBoolean(
+    struct java_lang_Object *self,
+    double v)
+{
+  return isnan(v);
+}
+
+bool method__Ojava_Dlang_DFloat_MisInfinite_Ascala_DFloat_Rscala_DBoolean(
+    struct java_lang_Object *self,
+    float v)
+{
+  return isinf(v);
+}
+
+bool method__Ojava_Dlang_DDouble_MisInfinite_Ascala_DDouble_Rscala_DBoolean(
+    struct java_lang_Object *self,
+    double v)
+{
+  return isinf(v);
+}
