a193ccde56ddc81189d1512b3e0ef46bcd25d00c
Geoff Reedy <geoff@programmer-monk.net>
2011-06-24 11:50:02 -0600

    add simplified predef

---
+++ docs/examples/llvm/Predef.scala
@@ -0,0 +1,32 @@
+package scala
+
+object Predef {
+  def classOf[T]: Class[T] = null
+  type String        = java.lang.String
+  type Class[T]      = java.lang.Class[T]
+  def assert(assertion: Boolean) {
+    if (!assertion)
+      throw new java.lang.AssertionError("assertion failed")
+  }
+  def identity[A](x: A): A         = x    // @see `conforms` for the implicit version
+  def implicitly[T](implicit e: T) = e    // for summoning implicit values from the nether world
+  def locally[T](x: T): T  = x    // to communicate intent and avoid unmoored statements
+  // Deprecated
+
+  def error(message: String): Nothing = system.error(message)
+  sealed abstract class <:<[-From, +To] extends (From => To)
+  implicit def conforms[A]: A <:< A = new (A <:< A) { def apply(x: A) = x }
+  /** A type for which there is always an implicit value.
+   *  @see fallbackCanBuildFrom in Array.scala
+   */
+  class DummyImplicit
+  
+  object DummyImplicit {
+  
+    /** An implicit value yielding a DummyImplicit.
+     *   @see fallbackCanBuildFrom in Array.scala
+     */
+    implicit def dummyImplicit: DummyImplicit = new DummyImplicit
+  }
+  type ClassManifest[T] = scala.reflect.ClassManifest[T]
+}
