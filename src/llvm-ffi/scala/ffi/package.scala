12adf38229b8de236045c85e0636803f41769f16
Geoff Reedy <geoff@programmer-monk.net>
2011-06-24 11:48:26 -0600

    add missing ffi package object

---
+++ src/llvm-ffi/scala/ffi/package.scala
@@ -0,0 +1,12 @@
+package scala
+
+package object ffi {
+  type CChar = ctypes.CChar
+  type CShort = ctypes.CShort
+  type CInt = ctypes.CInt
+  type CLong = ctypes.CLong
+  type CLongLong = ctypes.CLongLong
+  type CIntPtr = ctypes.CIntPtr
+  type CIntMax = ctypes.CIntMax
+  type CSizeT = ctypes.CSizeT
+}
