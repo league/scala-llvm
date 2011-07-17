b1f0b14819274bc815c4d68486bb88368a044391
Christopher League <league@contrapunctus.net>
2011-07-17 10:07:02 -0400

    Fix to use CSizeT instead of Int in ffi/alloc.scala

--- src/llvm-ffi/scala/ffi/alloc.scala
+++ src/llvm-ffi/scala/ffi/alloc.scala
@@ -5,7 +5,7 @@ object alloc {
   @foreign("malloc") private def _malloc(n: CSizeT): CIntPtr = x
   @foreign("free") private def _free(p: CIntPtr): Unit = x
   def malloc[T:Storable]: Ptr[T] = { mallocBytes(Storable.get[Ptr[T]].size) }
-  def mallocBytes[T](n: Int): Ptr[T] = Ptr.wrap(_malloc(n.toLong))
+  def mallocBytes[T](n: CSizeT): Ptr[T] = Ptr.wrap(_malloc(n))
   def free(p: Ptr[_]) = _free(Ptr.unwrap(p))
   def alloca[A:Storable,B](f: Ptr[A] => B): B = {
     val ptr: Ptr[A] = malloc
